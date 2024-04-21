9f8930314e3299c6b7e23ecb98bb6992d1290abe
atulya1028 <atulyaagrawal1996@gmail.com>
2024-04-21 19:41:34 +0530

    Design Change 21 April 2024

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "./Layout";
+import "../styles/Detail.css";
+import favorite from "../images/favorite.png";
+import share from "../images/share.png";
 
 const BookDetails = () => {
   const param = useParams();
@@ -8,6 +11,18 @@ const BookDetails = () => {
 
   const [book, setBook] = useState(null);
   const apiUrl = `http://localhost:3000/api/books/${param.id}`;
+
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  const [readMore, setReadMore] = useState(false);
+
+  const handleReadMore = () => {
+    setReadMore(!readMore);
+  };
 
   useEffect(() => {
     fetch(apiUrl)
@@ -19,7 +34,68 @@ const BookDetails = () => {
   return (
     <>
       <Layout />
-      <div>
+      {book ? (
+        <>
+          <div className="detail-container">
+            <div className="block">
+              <img
+                src={`http://localhost:3000/${book.image}`}
+                alt={book.title}
+                className="detail-image"
+              />
+              <div className="sub-block">
+                <div className="block2">
+                  <h1 className="title">{book.title}</h1>
+                  <h6 className="author">{book.author}</h6>
+                </div>
+                <div className="block3">
+                  <h2>{book.price}</h2>
+                  <span>
+                    <img src={favorite} alt="favorite" width={20} />
+                  </span>
+                </div>
+                <select
+                  value={selectedValue}
+                  onChange={handleChange}
+                  className="drop-down"
+                >
+                  {[...Array(10).keys()].map((num) => (
+                    <option key={num + 1} value={num + 1}>
+                      {num + 1}
+                    </option>
+                  ))}
+                </select>
+                <div className="btn-setting">
+                  <div className="btn-handle">
+                    <button className="cart-btn">ADD TO CART</button>
+                    <button className="buy-btn">ADD TO CART</button>
+                  </div>
+                </div>
+                <div className="desc-setting">
+                  <h2>Description</h2>
+                  <div style={{ display: "flex" }}>
+                    <div>
+                      {readMore ? (
+                        <p className="desc">
+                          {book.description.slice(0, 100)}...
+                        </p>
+                      ) : (
+                        <p className="desc">{book.description}</p>
+                      )}
+                      <div onClick={handleReadMore} className="read">
+                        {readMore ? "View More" : "View Less"}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+      {/*  <div>
         {book ? (
           <>
             <div className="detail-container">
@@ -58,7 +134,7 @@ const BookDetails = () => {
         ) : (
           <p>Loading...</p>
         )}
-      </div>
+      </div> */}
     </>
   );
 };
